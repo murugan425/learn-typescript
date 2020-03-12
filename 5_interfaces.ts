73d7bbd50814164c3e06d4b6ffd5a65575a36c07
murugan425 <murugan425@gmail.com>
2020-03-13 00:15:14 +0530

    purpose of using a interface in typescript.

--- 5_interfaces.ts
+++ 5_interfaces.ts
@@ -3,6 +3,10 @@ interface User {
     mobile: number;
 }
 
+//In the below case we are using multiple parameters which can be 
+//consolidated into one user defined type using a interface
+let printsomevariable = (x: string, y:number, z: number, a, b, c, d) => {}
+
 let printUserDetails = (user: User) => {
     console.log(user.fullname);
     console.log(user.mobile);
