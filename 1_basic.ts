341579b4076f036c6271824a52da04aec27d07d0
murugan425 <murugan425@gmail.com>
2020-03-13 00:00:47 +0530

    basic js function

---
+++ 1_basic.ts
@@ -0,0 +1,12 @@
+function showMsg() {    
+    //document.getElementById("msgId").innerHTML = "Index file return this text to try execution of TS/JS snippet in lite-server";
+    console.log('test');
+};
+
+//You can try ScratchJs extension in Google Chrome or Typescript Playground 
+//to see the ES5 equivalent code(JS) transpiled(Typescript is a Transpiler) 
+//from any ES6 code.
+
+//Future, most of the browsers are supposed to support ES6 standards/specifications
+
+showMsg();
